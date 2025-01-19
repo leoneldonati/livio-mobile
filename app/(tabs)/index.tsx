4e3beba921944fd50f99cffb99000d42b3db5124
Leonel Donati <leonelroman321@gmail.com>
2025-01-19 16:42:04 -0300

    created add post btn

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Pressable>
+        <Ionicons
+          name="add-circle"
+          size={62}
+          color="rgb(134,290,110)"
+          style={styles.addBtn}
+        />
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: "relative",
+    height: 100,
+  },
+  addBtn: {
+    backgroundColor: "#000",
+    borderRadius: 100,
+    position: "absolute",
+    right: 20,
+    bottom: -700,
+  },
+});
